0134f3c37abd3746868fa71fb99564051d388b6f
Allan Castro <allan.ocastrocruz@gmail.com>
2021-03-20 23:27:21 -0600

    bd

--- bd/gantt.sql
+++ bd/gantt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 19, 2021 at 09:26 PM
+-- Generation Time: Mar 21, 2021 at 06:21 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -41,16 +41,20 @@ CREATE TABLE `actividades` (
   `materia_prima` double NOT NULL,
   `depreciacion` double NOT NULL,
   `gastos_admi` double NOT NULL,
-  `entregables` tinytext NOT NULL
+  `entregables` tinytext NOT NULL,
+  `observaciones` text NOT NULL,
+  `creado_el` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `actividades`
 --
 
-INSERT INTO `actividades` (`idact`, `tarea`, `responsable`, `inicio`, `fin`, `avance`, `mano_obra`, `materia_prima`, `depreciacion`, `gastos_admi`, `entregables`) VALUES
-(1, 'Mantenimiento de herramienta', 1, '2021-03-22 09:00:00', '2021-03-24 09:00:00', 0, 3453453, 6547567, 3453453, 5675675, 'Reporte de actividades'),
-(2, 'Compra de materia prima', 2, '2021-03-24 09:00:00', '2021-03-26 16:00:00', 10, 3453453, 6547567, 3453453, 5675675, 'Facturas');
+INSERT INTO `actividades` (`idact`, `tarea`, `responsable`, `inicio`, `fin`, `avance`, `mano_obra`, `materia_prima`, `depreciacion`, `gastos_admi`, `entregables`, `observaciones`, `creado_el`) VALUES
+(1, 'Mantenimiento de herramienta', 1, '2021-03-22 09:00:00', '2021-03-24 09:00:00', 0, 3453453, 6547567, 3453453, 5675675, 'Reporte de actividades', 'Todo bien', '2021-03-20 15:22:57'),
+(2, 'Compra de material', 2, '2021-03-24 09:00:00', '2021-03-26 16:00:00', 10, 3453453, 6547567, 3453453, 5675675, 'Facturas', 'Entrega tardia', '2021-03-20 15:22:57'),
+(4, 'Procesamiento de materia', 2, '2021-03-25 21:15:00', '2021-04-02 21:15:00', 10, 4345, 3453453, 4534534, 5345345, 'Facturas', 'Ninguna', '2021-03-20 21:15:33'),
+(5, 'Procesamiento de materia', 2, '2021-03-20 21:22:00', '2021-04-02 21:22:00', 23, 3453, 45345, 234525, 345, 'Facturas', 'Ninguna', '2021-03-20 21:22:28');
 
 -- --------------------------------------------------------
 
@@ -66,18 +70,19 @@ CREATE TABLE `usuarios` (
   `apemat` varchar(20) NOT NULL,
   `puesto` varchar(30) NOT NULL,
   `correo` tinytext NOT NULL,
-  `direccion` text NOT NULL
+  `direccion` text NOT NULL,
+  `cont` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `usuarios`
 --
 
-INSERT INTO `usuarios` (`idus`, `nombre`, `apepat`, `apemat`, `puesto`, `correo`, `direccion`) VALUES
-(1, 'Miguel', 'Cruz ', 'Ramos', 'Admin', 'i16221966.19@puebla.tecnm.mx', '3262  Hidden Meadow Drive'),
-(2, 'Mary ', 'Martinez', 'Salas', 'Admin', 'tysoq3aap2@temporary-mail.net', '1583  Dark Hollow Road'),
-(3, 'Paul', 'Boucher', 'Rodriguez', 'Empleado', 'auwznmqwq6b@temporary-mail.net', '2745  Capitol Avenue'),
-(4, 'Deborah', 'Bass', 'Yu', 'Empleada', '5rf3k5h21yu@temporary-mail.net', '2439  Southside Lane');
+INSERT INTO `usuarios` (`idus`, `nombre`, `apepat`, `apemat`, `puesto`, `correo`, `direccion`, `cont`) VALUES
+(1, 'Miguel', 'Cruz ', 'Ramos', 'Admin', 'i16221966.19@puebla.tecnm.mx', '3262  Hidden Meadow Drive', '$2y$10$Yf1.VCUQwsRgzn0SZAVzBeKOkz4JGP2vbOYspgXogKowHvIUWh/Ye'),
+(2, 'Mary ', 'Martinez', 'Salas', 'Admin', 'mary@email.com', '1583  Dark Hollow Road', '$2y$10$mIc1bmK/84si74TdJxBbQe/OvJ267apcvAh6RmbZui4oqlew0O2Aa'),
+(3, 'Paul', 'Boucher', 'Rodriguez', 'Empleado', 'auwznmqwq6b@temporary-mail.net', '2745  Capitol Avenue', '$2y$10$Yf1.VCUQwsRgzn0SZAVzBeKOkz4JGP2vbOYspgXogKowHvIUWh/Ye'),
+(4, 'Deborah', 'Bass', 'Yu', 'Empleada', '5rf3k5h21yu@temporary-mail.net', '2439  Southside Lane', '$2y$10$Yf1.VCUQwsRgzn0SZAVzBeKOkz4JGP2vbOYspgXogKowHvIUWh/Ye');
 
 --
 -- Indexes for dumped tables
@@ -104,7 +109,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT for table `actividades`
 --
 ALTER TABLE `actividades`
-  MODIFY `idact` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idact` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `usuarios`
